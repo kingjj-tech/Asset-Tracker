2039c00de657385c0bdf6a998fdd50f0c78d4def
CloudyJayC <jwcameron3@gmail.com>
2024-07-01 16:31:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-const http = require("http");
-const fs = require("fs");
-const port = 3000
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
 
-const server = http.createServer(function (req, res){
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.readFile('index.html', function (error, data){
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', function (error, data) {
         if (error) {
-            res.writeHead(404)
-            res.write("Error: File not found")
+            res.writeHead(404);
+            res.write('Error: File not found');
         } else {
-            res.write(data)
+            res.write(data);
         }
-        res.end()
-    })
-})
+        res.end();
+    });
+});
 
 server.listen(port, function () {
-    if(error) {
-        console.log("Something went wrong", error)
-    }else{
-        console.log("Serrver is listening on port"+ port)
-    }
-})
+    console.log('Server is listening on port ' + port);
+});
