e3d428634a7b5a1cd6737cfe5a3e2bdb96004193
CloudyJayC <jwcameron3@gmail.com>
2024-07-01 18:29:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const AssetHistory = require('./models/assetHistory');
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/asset_management', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-});
+mongoose.connect('mongodb://localhost:27017/asset_management');
 
 mongoose.connection.on('connected', () => {
   console.log('Connected to MongoDB');
@@ -25,6 +21,25 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Asset Tracking and Management API');
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(201).send(newUser);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(port, () => {
