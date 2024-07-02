11632a0e4d9d8594ff91087424ace6e541e927d0
CloudyJayC <jwcameron3@gmail.com>
2024-07-02 07:19:59 +0200

    Updated server.js
    removed useNewUrlParser and useUnifieldTopology as they are functions getting removed from node.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/asset_management', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/asset_management');
 
 // Event listener for successful MongoDB connection
 mongoose.connection.on('connected', () => {
