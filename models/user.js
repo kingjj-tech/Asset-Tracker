4526162e7579e547e68a037321f424f03cdfad52
CloudyJayC <jwcameron3@gmail.com>
2024-07-03 18:45:22 +0200

    Functional login
    still needs proper CRUD operations for Postman to work

--- models/user.js
+++ models/user.js
@@ -12,15 +12,6 @@ const userSchema = new mongoose.Schema({
   tokens: [{ token: { type: String, required: true } }]
 });
 
-// Hash the plain text password before saving
-userSchema.pre('save', async function (next) {
-  const user = this;
-  if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 8);
-  }
-  next();
-});
-
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
